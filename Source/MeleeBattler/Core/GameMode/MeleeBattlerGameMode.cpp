e212a6d5f04917697a010c482dcbfe5b6de43506
DenisGonchar <denisgonchar123@gmail.com>
2023-08-14 23:07:04 +0300

    Initial commit

---
+++ Source/MeleeBattler/Core/GameMode/MeleeBattlerGameMode.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MeleeBattlerGameMode.h"
+#include "UObject/ConstructorHelpers.h"
+
+AMeleeBattlerGameMode::AMeleeBattlerGameMode()
+{
+	
+}
